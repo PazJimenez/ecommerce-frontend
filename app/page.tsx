717320fb248310beed2ec44289595b310b3a6132
PazJimenez <a.cornejo20@alumnos.santotomas.cl>
2025-09-13 20:44:55 -0300

    Se termina de configurar featured-products

--- app/page.tsx
+++ app/page.tsx
@@ -7,6 +7,7 @@ export default function Home() {
     <main>
       <CarouselTextBanner />
       <FeaturedProducts />
+      <BannerDiscount />
     </main>
   );
 }
--- components/featured-products.tsx
+++ components/featured-products.tsx
@@ -3,16 +3,19 @@
 
 import { useGetFeaturedProducts } from "@/api/useGetFeaturedProducts";
 import { ResponseType } from "@/types/response";
-import { Carousel, CarouselContent, CarouselItem } from "./ui/carousel";
+import { Carousel, CarouselContent, CarouselItem, CarouselNext, CarouselPrevious } from "./ui/carousel";
 import SkeletonSchema from "./skeletonScheme";
 import { ProductType } from "@/types/product";
 import { Card, CardContent } from "./ui/card";
+import { Expand, ShoppingCart } from "lucide-react";
+import IconButton from "./icon-button";
+import { useRouter } from "next/navigation";
 
 const FeaturedProducts = () => {
 
     const {result, loading}: ResponseType = useGetFeaturedProducts();
 
-    console.log("result", result);
+    const router = useRouter();
 
     return (
         <div className="max-w-6xl py-4 mx-auto sm:py-16 sm:px-24">
@@ -24,22 +27,46 @@ const FeaturedProducts = () => {
                     )}
                     {result != null && (
                         result.map((product: ProductType) => {
-                            const {slug, images, productName, colour, material, id } = product
+                            const {slug, images, productName, colour, material, id, category: {categoryName} } = product
 
                             return (
                                 <CarouselItem key={id} className="md:basis-1/2 lg:basis-1/3 group">
                                     <Card className="py-4 border border-gray-200 shadow-none">
                                         <CardContent className="relative flex items-center justify-center px-6 py-2">
                                             <img src={`${process.env.NEXT_PUBLIC_BACKEND_URL}${images[0].url}`} 
-                                            alt="image featured" 
-                                            />
+                                            alt="image featured" />
+                                            <div className="absolute w-full px-6 transition duration-200 opacity-0 group-hover:opacity-100 bottom-5">
+                                                <div className="flex justify-center gap-x-6">
+                                                    <IconButton 
+                                                        onClick={() => router.push(`product/${slug}`)} 
+                                                        icon={<Expand size={20}/>}
+                                                        className="text-gray-600"/>
+
+                                                    <IconButton 
+                                                        onClick={() => console.log("Add item")} 
+                                                        icon={<ShoppingCart size={20}/>}
+                                                        className="text-gray-600"/>
+                                                    
+                                                    
+
+                                                </div>
+                                            </div>
                                         </CardContent>
+                                        <div className="flex justify-between gap-4 px-8">
+                                            <h3 className="text-lg font-bold">{productName}</h3>
+                                            <div className="flex items-center justify-between gap-3">
+                                                <p className="px-2 py-1 text-white bg-black rounded-full dark:bg-white text-black w-fit">{material}</p>
+                                                <p className="px-2 py-1 text-white bg-yellow-900 rounded-full w-fit">{categoryName}</p>
+                                            </div>
+                                        </div>
                                     </Card>
                                 </CarouselItem>
                             )
                         })
                     )}
                 </CarouselContent>
+                <CarouselPrevious />
+                <CarouselNext className="hidden sm:flex"/>
             </Carousel>
         </div>
     )
